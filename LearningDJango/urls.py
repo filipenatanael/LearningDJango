633758dcf1d8b0381a5825ffb1445086b97951e0
Filipe Natanael <filipenatanael@live.com>
2017-09-06 23:50:57 -0300

    Urls
    Urls

--- LearningDJango/urls.py
+++ LearningDJango/urls.py
@@ -19,7 +19,6 @@ from .core import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', views.home, name='home'),
-    url(r'^contact', views.contact, name='contact'),
+    url(r'^', include('LearningDJango.core.MyURLS')),
 ]
 
