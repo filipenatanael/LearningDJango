7280ee2a74f0dac509915e34ea8cfa7c50c74020
Filipe Natanael <filipenatanael@live.com>
2017-10-01 22:54:16 -0300

    Changes in class Course
    Changes in class Course and Changed files

--- LearningDJango/Courses/models.py
+++ LearningDJango/Courses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField('Name',max_length=100)
+    slug = models.SlugField('Shortcut')
+    description = models.TextField('Description', blank=True)
+    start_date = models.DateField('Data Starting', null=True, blank=True)
+    image = models.ImageField(upload_to='courses/images', verbose_name='Images', null=True, blank=True)
+
+    created_at = models.DateTimeField('Created in', auto_now_add=True)
+    updated_at = models.DateTimeField('Updated at', auto_now_add=True)
