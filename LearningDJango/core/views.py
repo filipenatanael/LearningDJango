eab148492228f376b55cbe61be8798d64b8cabf7
Filipe Natanael <filipenatanael@live.com>
2017-10-08 22:43:58 -0300

    First commit today
    First commit today

--- LearningDJango/core/views.py
+++ LearningDJango/core/views.py
@@ -6,6 +6,7 @@ def home(request):
     return render(request, 'home.html', {'username': 'Filipe'})
     # return HttpResponse('Hello World!!!')
 
-
 def contact(request):
-    return render(request, 'contact.html',  {'email': 'LearningHotmail@homail.com'})
+    emails = ['Learning@homail.com','Learning@gmail.com']
+    return render(request, 'contact.html', emails)
+    #return render(request, 'contact.html',  {'email':'email@exemple.com'})
