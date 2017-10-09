e5abbb43024866888eb78842ae957f665d3f75e5
Filipe Natanael <filipenatanael@live.com>
2017-10-08 23:17:35 -0300

    New template
    Created a new templete to show courses

--- LearningDJango/core/MyURLS.py
+++ LearningDJango/core/MyURLS.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^home/', views.home, name='home'),
     url(r'^contact', views.contact, name='contact'),
+    url(r'^courses', views.courses, name='courses'),
 ]
--- LearningDJango/core/views.py
+++ LearningDJango/core/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from LearningDJango.Courses.models import Course
 
 def home(request):
     return render(request, 'home.html', {'username': 'Filipe'})
     # return HttpResponse('Hello World!!!')
 
 def contact(request):
-    emails = ['Learning@homail.com','Learning@gmail.com']
-    return render(request, 'contact.html', emails)
-    #return render(request, 'contact.html',  {'email':'email@exemple.com'})
+    return render(request, 'contact.html',  {'email': 'email@exemple.com'})
+
+def courses(request):
+    course = Course.objects.all()
+    template_name = 'courses.html'
+    context = {
+        'courses': course
+    }
+    return render(request, template_name, context)
